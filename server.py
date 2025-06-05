7274204226327da04542695ebb8a6516b32cc4d7
Euclidae <typicravager@gmail.com>
2025-06-05 15:12:14 +0200

    ad

--- server.py
+++ server.py
@@ -8,9 +8,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#internet version 4, sec
 sock.bind((host,port))
 
 sock.listen(1)
+print("The server is running and is listening to client request")
 conn, address = sock.accept()
 
 message = "Hey, there is something important for you."
-conn.send(message,encode())
+conn.send(message,message.encode())
 
 conn.close()
